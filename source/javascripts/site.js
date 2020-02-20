548a35fae91bb8b2786d75b65e0107ba0ce8fdd4
TatsuroMoriyama <t4traw@gmail.com>
2020-02-20 22:26:41 +0900

    簡単なjavascriptでのメニュー開閉

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
 // This is where it all goes :)
+
+function clickMenu(el) {
+  let menu = document.getElementById("navbarBasicExample");
+  menu.classList.toggle("is-active");
+  el.classList.toggle("is-active");
+}
